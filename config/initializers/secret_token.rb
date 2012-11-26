7f761ed2e22ac9b58b570657af4ddc8862f2f353
Serik Beketayev <serik.beketayev@gmail.com>
2012-11-26 22:01:21 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharityPlatform::Application.config.secret_token = 'da434f59a4be22247cad0a1f6a81f5cc95a02aa561b71c277fb307f0b714fbccf6132f1e92493a3e172192c6cf3ec1519ff05b4cfb9773f336daa012771347af'
